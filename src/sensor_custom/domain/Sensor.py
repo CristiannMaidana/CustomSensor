ba139371e1e3018b9100a3f0d5baab2437ff3032
CristiannMaidana <cristian.mg.david@gmail.com>
2026-02-04 17:14:01 -0300

    feat(Sensor): crear clase
    - Crear directorios 'src', 'sensor_custom' y 'domain'
    
    - Crear clase 'Sensor', con propiedades: id_sensor, nombre_sensor, fecha_instalacion_sensor, numero_serie y id_contenedor
    
    - Clase para inicializar con datos del sensor cargado en base de datos

---
+++ src/sensor_custom/domain/Sensor.py
@@ -0,0 +1,7 @@
+class Sensor:
+    def __init__(self, id_sensor, nombre_sensor, fecha_instalacion_sensor, numero_serie, id_contenedor,):
+        self.id_sensor = id_sensor
+        self.nombre_sensor = nombre_sensor
+        self.fecha_instalacion_sensor = fecha_instalacion_sensor
+        self.numero_serie = numero_serie
+        self.id_contenedor = id_contenedor
